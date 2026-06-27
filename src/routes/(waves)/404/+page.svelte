04ba283f74c300fc24beaf44645032b7514c4849
jaweed3 <wedjaw22@gmail.com>
2026-06-27 12:56:29 +0700

    chore: app HTML shell, 404 page

--- src/routes/(waves)/404/+page.svelte
+++ src/routes/(waves)/404/+page.svelte
@@ -1,41 +1,58 @@
 <script>
-	import Button from '$lib/components/atoms/Button.svelte';
-	import Error from '$lib/icons/error.svelte';
+  import Button from '$lib/components/atoms/Button.svelte';
+  import Error from '$lib/icons/error.svelte';
 </script>
 
 <div class="error-page">
-	<div class="container">
-		<h1>Oh no! 404!</h1>
-		<div class="svg-wrapper">
-			<Error />
-		</div>
-		<p>It seems like coffee was spilled all over this page, and now it can't be displayed.</p>
-		<br />
-		<Button href="/">Start over</Button>
-	</div>
+  <div class="container">
+    <h1>404 — Page Not Found</h1>
+    <div class="svg-wrapper">
+      <Error />
+    </div>
+    <p>It seems like coffee was spilled all over this page, and now it can't be displayed.</p>
+    <br />
+    <Button href="/">Start over</Button>
+  </div>
 </div>
 
 <style lang="scss">
-	.error-page {
-		background: var(--color--page-background);
-		position: relative;
-	}
-	.container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		min-height: 60vh;
-		text-align: center;
+  .error-page {
+    position: relative;
+    min-height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 0 var(--gutter);
 
-		.svg-wrapper {
-			width: 300px;
-			margin-top: -60px;
-			margin-bottom: -30px;
+    h1 {
+      font-size: var(--headline-lg-mobile);
+      color: var(--accent);
 
-			:global(svg) {
-				filter: drop-shadow(2px 6px 0px rgba(0, 0, 0, 0.1));
-			}
-		}
-	}
+      @media (min-width: 768px) {
+        font-size: var(--headline-lg);
+      }
+    }
+
+    p {
+      color: var(--muted);
+      max-width: 480px;
+    }
+
+    .svg-wrapper {
+      width: 300px;
+      margin-top: -60px;
+      margin-bottom: -30px;
+
+      :global(svg) {
+        filter: drop-shadow(2px 6px 0px rgba(0, 0, 0, 0.1));
+      }
+    }
+  }
 </style>
