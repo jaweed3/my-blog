bcc7bd2cc43fd9d153c669fb4e1c9f2b0fbadafa
jaweed3 <wedjaw22@gmail.com>
2026-07-14 07:29:37 +0700

    fix: update domain jaweed.site → wedjaw.my.id

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,7 +2,7 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://jaweed.site/';
+export const siteBaseUrl = 'https://wedjaw.my.id/';
 
 export const keywords = [
 	'Edge ML',
